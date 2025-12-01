46813d6f59198afeba4b30012a6fad17bbed62b0
Juan Meregone <juanmeregone@gmail.com>
2025-12-01 11:52:53 -0300

    changes in XLSX

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const fullBase = async (req,res)=> {
 
 const tickets = async(req,res) => {
     const workbook = await XlsxPopulate.fromFileAsync('./Tickets.xlsx')
-    const value = workbook.sheet('query (1)').usedRange().value()
+    const value = workbook.sheet('Sheet1').usedRange().value()
     res.json(value)
 }
 export  {tickets,fullBase}
